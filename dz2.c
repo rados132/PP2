d0f9dfd3494a9704cc6ec0b808cc969bc8ba1659
Radoš Radovanović <rados@MacBook.local>
2023-04-18 14:35:31 +0200

    Gotovo ali ne radi skroz

--- dz2.c
+++ dz2.c
@@ -9,7 +9,7 @@ int main(void)
     {
         return 0;
     }
-    int **matrica = malloc(m * sizeof(int *));
+    int **matrica = calloc(m, sizeof(int *));
     if (matrica == NULL)
     {
         printf("GRESKA");
@@ -17,7 +17,7 @@ int main(void)
     }
     for (int i = 0; i < m; i++)
     {
-        matrica[i] = malloc(n * sizeof(int));
+        matrica[i] = calloc(n, sizeof(int));
         if (matrica[i] == NULL)
         {
             printf("GRESKA");
@@ -51,6 +51,46 @@ int main(void)
     {
         return 0;
     }
+    int s, max_suma, pozicija[2];
+    max_suma = 0;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            s = 0;
+            for (int p = 0; p < k && i + p < m; p++)
+            {
+                for (int q = 0; q < k && j + q < n; q++)
+                {
+                    s += matrica[i + p][j + q];
+                }
+            }
+            if (s > max_suma)   
+            {
+                max_suma = s;
+                pozicija[0] = i;
+                pozicija[1] = j;
+            }
+        }  
+    }
+    for (int i = pozicija[0]; i < pozicija[0] + k; i++)
+    {
+        for (int j = pozicija[1]; j < pozicija[1] + k; j++)
+        {
+            if (j != pozicija[1] + k - 1)
+            {
+                printf("%d ", matrica[i][j]);
+            }
+            else
+            {
+                printf("%d", matrica[i][j]);
+            }
+        }
+        if (i != pozicija[0] + k - 1)
+        {
+            printf("\n");
+        }
+    }   
     for (int i = 0; i < m; i++)
     {
         free(matrica[i]);
