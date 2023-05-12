6f2ba73a010c083084edecc053d853e180e777dc
Rados Radovanovic <radosradovanovic@gmail.com>
2023-05-12 20:13:13 +0200

    char** readLines(int *n) done

--- dz3.c
+++ dz3.c
@@ -7,9 +7,11 @@ char** readLines(int *n);
 
 int main(void)
 {
-    int pwr;
-    char *line;
+    //stdin = fopen("input.txt", "r");
+    int pwr, n = 0;
+    char **lines;
     scanf("%d", &pwr);
+    getchar();
     switch (pwr)
     {
         case -3:
@@ -28,28 +30,80 @@ int main(void)
             printf("GRESKA");
             return 0;
     }
-    // line = readLine();
-    // printf("%s", line);
-    free(line);
+    lines = readLines(&n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%s", lines[i]);
+        if (i != n - 1)
+        {
+            putchar('\n');
+        }
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        free(lines[i]);
+    }
+    free(lines);
     return 0;
 }
 
-char *readLine()
+char* readLine()
 {
-    char c, *line;
-    line = malloc(sizeof(char));
-    if (line == NULL)
+    char *line, buffer[100];
+    int len;
+    if (fgets(buffer, 100, stdin) != NULL)
+    {
+        len = strlen(buffer);
+        if (buffer[len - 1] == '\n')
+        {
+            buffer[len - 1] = '\0';
+        }
+        line = malloc(len * sizeof(char));
+        if (line == NULL)
+        {
+            printf("MEM_GRESKA");
+            exit(0);
+        }
+        strcpy(line, buffer);
+        return line;
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+char** readLines(int *n)
+{
+    char **lines;
+    lines = malloc(sizeof(char *));
+    if (lines == NULL)
     {
         printf("MEM_GRESKA");
         exit(0);
     }
     int i = 0;
-    getchar();
-    while ((c = getchar()) != '\n')
+    char *tmp;
+    while (1)
     {
-        line[i++] = c;
-        line = realloc(line, (i + 1) * sizeof(char));
+        tmp = readLine();
+        if (tmp == NULL)
+        {
+            break;
+        }
+        lines[i++] = tmp;
+        lines = realloc(lines, (i + 1) * sizeof(char *));
+        if (lines == NULL)
+        {
+            printf("MEM_GRESKA");
+            for (int i = 0; i <= *n; i++)
+            {
+                free(lines[i]);
+            }
+            free(lines);
+            exit(0);
+        }
+        (*n)++;
     }
-    line[i] = '\0';
-    return line;
+    return lines;
 }
