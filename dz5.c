2816f9ad8c211cfe25f471cffce605ee2b2ae4db
Rados Radovanovic <radosradovanovic@gmail.com>
2023-05-25 23:18:26 +0200

    ll done

--- dz5.c
+++ dz5.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX 100
 
 typedef struct node
 {
-    char **contact;
+    char *info;
     struct node *next;
 }
 AddressBook;
 
-char** readLine(FILE *input);
+char* readRow(FILE *input);
 AddressBook* readFile(char *inputFile);
 
 int main(int argc, char *argv[])
@@ -30,17 +31,66 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-char** readLine(FILE *input)
+char* readRow(FILE *input)
 {
-    char buffer[MAX];
-    fgets(buffer, MAX, input);
-    
+    char buffer[MAX], *row;
+    if (fgets(buffer, MAX, input) != NULL)
+    {
+        row = calloc(strlen(buffer), sizeof(char));
+        if (row == NULL)
+        {
+            printf("MEM_GRESKA");
+            exit(0);
+        }
+        strcpy(row, buffer);
+        return buffer;
+    }
+    else
+    {
+        return NULL;
+    }
 }
 
 AddressBook* readFile(char *inputFile)
 {
     FILE *input;
     input = fopen(inputFile, "r");
-
+    if (input = NULL)
+    {
+        printf("DAT_GRESKA");
+        exit(0);
+    }
+    AddressBook *head, *current, *tmp;
+    head = malloc(sizeof(AddressBook));
+    if (head == NULL)
+    {
+        printf("MEM_GRESKA");
+        exit(0);
+    }
+    head->next = NULL;
+    current = head;
+    char *row;
+    int contactsCnt = 0;
+    while (row = readRow(input), row != NULL)
+    {
+        if ((contactsCnt++) == 0)
+        {
+            current->info = row;
+        }
+        else
+        {
+            tmp = malloc(sizeof(AddressBook));
+            if (tmp == NULL)
+            {
+                printf("MEM_GRESKA");
+                exit(0);
+            }
+            tmp->info = row;
+            tmp->next = NULL;
+            current->next = tmp;
+            current = tmp;
+        }
+    }
     fclose(input);
+    return head;
 }
