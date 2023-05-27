8e362607204df67942b192c80230f88b4f58c330
Rados Radovanovic <radosradovanovic@gmail.com>
2023-05-27 19:36:38 +0200

    update

--- dz5.c
+++ dz5.c
@@ -14,7 +14,8 @@ AddressBook;
 char** readRow(FILE *input);
 AddressBook* readFile(char *inputFile);
 void removeContacts(AddressBook *head, char *displayName);
-void writeFile(AddressBook *head);
+void writeFile(AddressBook *head, char *outFile);
+void freeMemory(AddressBook *head);
 
 int main(int argc, char *argv[])
 {
@@ -23,21 +24,13 @@ int main(int argc, char *argv[])
         printf("ARG_GRESKA");
         exit(0);
     }
-    FILE *input = fopen("ulaz.csv", "r");
-    char **row = readRow(input);
-    fclose(input);
-    FILE *output = fopen("output.txt", "w");
-    for (int i = 0; i < 37; i++)
+    AddressBook *addressBook = readFile(argv[1]);
+    if (argc == 4)
     {
-        fprintf(output, "%s\n", row[i]);
+        removeContacts(addressBook, argv[3]);
     }
-    fclose(output);
-    // AddressBook *addressBook = readFile(argv[1]);
-    // writeFile(addressBook);
-    // if (argc = 4)
-    // {
-    //     removeContacts(addressBook, argv[3]);
-    // }
+    writeFile(addressBook, argv[2]);
+    freeMemory(addressBook);
     return 0;
 }
 
@@ -48,12 +41,12 @@ char** readRow(FILE *input)
     if (row == NULL)
     {
         printf("MEM_GRESKA");
+        fclose(input);
         exit(0);
     }
     if (fgets(buffer, BUFFER, input) != NULL)
     {
         int charCnt = 0, columnCnt = 0;
-        printf("%lu", strlen(buffer));
         for (int i = 0; i < strlen(buffer) - 1; i++, charCnt++)
         {
             if (buffer[i] == ',')
@@ -62,17 +55,19 @@ char** readRow(FILE *input)
                 if (row == NULL)
                 {
                     printf("MEM_GRESKA");
+                    fclose(input);
                     exit(0);
                 }
                 row[columnCnt] = calloc(charCnt, sizeof(char));
                 if (row[columnCnt] == NULL)
                 {
                     printf("MEM_GRESKA");
+                    fclose(input);
                     exit(0);
                 }
                 if (i != 0 && buffer[i - 1] == ',')
                 {
-                    row[columnCnt][0] = '\0';
+                    row[columnCnt] = NULL;
                 }
                 else
                 { 
@@ -106,11 +101,17 @@ AddressBook* readFile(char *inputFile)
         printf("DAT_GRESKA");
         exit(0);
     }
+    if (readRow(input) == NULL)
+    {
+        fclose(input);
+        return NULL;
+    }
     AddressBook *head, *current, *tmp;
     head = malloc(sizeof(AddressBook));
     if (head == NULL)
     {
         printf("MEM_GRESKA");
+        fclose(input);
         exit(0);
     }
     head->next = NULL;
@@ -129,6 +130,7 @@ AddressBook* readFile(char *inputFile)
             if (tmp == NULL)
             {
                 printf("MEM_GRESKA");
+                fclose(input);
                 exit(0);
             }
             tmp->info = row;
@@ -141,26 +143,175 @@ AddressBook* readFile(char *inputFile)
     return head;
 }
 
-// void removeContacts(AddressBook *head, char *displayName)
-// {
-//     for (AddressBook *tmp = head; tmp != NULL; tmp = tmp->next)
-//     {
-        
-//     }
-    
-// }
+void removeContacts(AddressBook *head, char *displayName)
+{
+    AddressBook *tmp = head, *previous = NULL;
+    while (tmp != NULL)
+    {
+        if (tmp == head && strstr(tmp->info[2], displayName) != NULL)
+        {
+            head = tmp->next;
+            free(tmp);
+            tmp = head;
+            previous = head;
+        }
+        else if (strstr(tmp->info[2], displayName) != NULL)
+        {
+            previous->next = tmp->next;
+            free(tmp);
+            tmp = previous->next;
+        }
+        else
+        {
+            previous = tmp;
+            tmp = tmp->next;
+        }
+    }      
+}
 
-void writeFile(AddressBook *head)
+void writeFile(AddressBook *head, char *outFile)
 {
     FILE *output;
-    output = fopen("output.txt", "w");
+    output = fopen(outFile, "w");
+    if (head == NULL)
+    {
+        fclose(output);
+    }
     for (AddressBook *tmp = head; tmp != NULL; tmp = tmp->next)
     {
-        for (int i = 0; i < 3; i++)
+        fprintf(output, "dn: cn=%s,mail=%s\n", tmp->info[2], tmp->info[4]);
+        fprintf(output, "objectclass: top\nobjectclass: person\nobjectclass: organizationalPerson\nobjectclass: inetOrgPerson\nobjectclass: mozillaAbPersonAlpha\n");
+        if (tmp->info[0] != NULL)
         {
-            printf("%s ", tmp->info[i]);
+            fprintf(output, "givenName: %s\n", tmp->info[0]);
         }
-        putchar('\n');
+        if (tmp->info[1] != NULL)
+        {
+            fprintf(output, "sn: %s\n", tmp->info[1]);
+        }
+        if (tmp->info[2] != NULL)
+        {
+            fprintf(output, "cn: %s\n", tmp->info[2]);
+        }
+        if (tmp->info[3] != NULL)
+        {
+            fprintf(output, "mozillaNickname: %s\n", tmp->info[3]);
+        }
+        if (tmp->info[4] != NULL)
+        {
+            fprintf(output, "mail: %s\n", tmp->info[4]);
+        }
+        if (tmp->info[5] != NULL)
+        {
+            fprintf(output, "mozillaSecondEmail: %s\n", tmp->info[5]);
+        }
+
+        fprintf(output, "modifytimestamp: 0\n");
+
+        if (tmp->info[7] != NULL)
+        {
+            fprintf(output, "telephoneNumber: %s\n", tmp->info[7]);
+        }
+        if (tmp->info[8] != NULL)
+        {
+            fprintf(output, "homePhone: %s\n", tmp->info[8]);
+        }
+        if (tmp->info[9] != NULL)
+        {
+            fprintf(output, "facsimiletelephonenumber: %s\n", tmp->info[9]);
+        }
+        if (tmp->info[10] != NULL)
+        {
+            fprintf(output, "pager: %s\n", tmp->info[10]);
+        }
+        if (tmp->info[11] != NULL)
+        {
+            fprintf(output, "mobile: %s\n", tmp->info[11]);
+        }
+        if (tmp->info[12] != NULL)
+        {
+            fprintf(output, "mozillaHomeStreet: %s\n", tmp->info[12]);
+        }
+        if (tmp->info[14] != NULL)
+        {
+            fprintf(output, "mozillaHomeLocalityName: %s\n", tmp->info[14]);
+        }
+        if (tmp->info[15] != NULL)
+        {
+            fprintf(output, "mozillaHomeState: %s\n", tmp->info[15]);
+        }
+        if (tmp->info[16] != NULL)
+        {
+            fprintf(output, "mozillaHomePostalCode: %s\n", tmp->info[16]);
+        }
+        if (tmp->info[17] != NULL)
+        {
+            fprintf(output, "mozillaHomeCountryName: %s\n", tmp->info[17]);
+        }
+        if (tmp->info[18] != NULL)
+        {
+            fprintf(output, "street: %s\n", tmp->info[18]);
+        }
+        if (tmp->info[20] != NULL)
+        {
+            fprintf(output, "l: %s\n", tmp->info[20]);
+        }
+        if (tmp->info[21] != NULL)
+        {
+            fprintf(output, "st: %s\n", tmp->info[21]);
+        }
+        if (tmp->info[22] != NULL)
+        {
+            fprintf(output, "postalCode: %s\n", tmp->info[22]);
+        }
+        if (tmp->info[23] != NULL)
+        {
+            fprintf(output, "c: %s\n", tmp->info[23]);
+        }
+        if (tmp->info[24] != NULL)
+        {
+            fprintf(output, "title: %s\n", tmp->info[24]);
+        }
+        if (tmp->info[25] != NULL)
+        {
+            fprintf(output, "ou: %s\n", tmp->info[25]);
+        }
+        if (tmp->info[26] != NULL)
+        {
+            fprintf(output, "o: %s\n", tmp->info[26]);
+        }
+        if (tmp->info[27] != NULL)
+        {
+            fprintf(output, "mozillaWorkUrl: %s\n", tmp->info[27]);
+        }
+        if (tmp->info[28] != NULL)
+        {
+            fprintf(output, "mozillaHomeUrl: %s\n", tmp->info[28]);
+        }
+        if (tmp->info[36] != NULL)
+        {
+            fprintf(output, "description: %s\n", tmp->info[36]);
+        }
+        if (tmp->next != NULL)
+        {
+            fprintf(output, "\n");
+        }       
     }
     fclose(output);
+}
+
+void freeMemory(AddressBook *head)
+{
+    AddressBook *tmp = NULL;
+    while (head != NULL)
+    {
+        tmp = head;
+        head = head->next;
+        for (int i = 0; i < 38; i++)
+        {
+            free(tmp->info[i]);
+        }
+        free(tmp->info);
+        free(tmp);
+    }
 }
